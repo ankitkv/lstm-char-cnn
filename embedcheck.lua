bff265fc9a511977554493294bb25b94ab952bb8
Ankit Vani <a@nevitus.org>
2016-07-01 19:19:13 -0400

    Make embedcheck interactable

--- embedcheck.lua
+++ embedcheck.lua
@@ -24,14 +24,12 @@ local stringx = require('pl.stringx')
 cmd = torch.CmdLine()
 cmd:text('Options')
 -- data
-cmd:option('-model', 'en-large-word-model.t7', 'model checkpoint file')
+cmd:option('-model', 'cv/lm_char.t7', 'model checkpoint file')
 -- GPU/CPU these params must be passed in because it affects the constructors
 cmd:option('-gpuid', 0,'which gpu to use. -1 = use CPU')
 cmd:option('-cudnn', 1,'use cudnn (1 = yes, 0 = no)')
 cmd:option('-save', 0,'create and save embeddings (1 = yes, 0 = no)')
 cmd:option('-embfile', 'embeddings', 'embeddings file')
-cmd:option('-k', 50, 'number of nearest neighbors')
-cmd:option('-knn', '', 'get the k nearest neighbors of words (comma-separated)')
 
 cmd:text()
 
@@ -108,22 +106,6 @@ if opt.gpuid >= 0 then
     for k,v in pairs(protos) do v:cuda() end
 end
 
-params, grad_params = model_utils.combine_all_parameters(protos.criterion, protos.rnn)
-print('number of parameters in the model: ' .. params:nElement())
-
--- for easy switch between using words/chars (or both)
-function get_input(x, x_char, t, prev_states)
-    local u = {}
-    if opt.use_chars == 1 then
-        table.insert(u, x_char[{{1,2},t}])
-    end
-    if opt.use_words == 1 then
-        table.insert(u, x[{{1,2},t}])
-    end
-    for i = 1, #prev_states do table.insert(u, prev_states[i]) end
-    return u
-end
-
 function get_embedding(word)
     local x = torch.ones(2, loader.max_word_l)
     local inword = opt.tokens.START .. word .. opt.tokens.END
@@ -156,58 +138,34 @@ function save_embeddings()
     torch.save(opt2.embfile, emb_table)
 end
 
-function Split(str, delim, maxNb)
-    -- Eliminate bad cases...
-    if string.find(str, delim) == nil then
-        return { str }
-    end
-    if maxNb == nil or maxNb < 1 then
-        maxNb = 0    -- No limit
-    end
-    local result = {}
-    local pat = "(.-)" .. delim .. "()"
-    local nb = 0
-    local lastPos
-    for part, pos in string.gfind(str, pat) do
-        nb = nb + 1
-        result[nb] = part
-        lastPos = pos
-        if nb == maxNb then break end
-    end
-    -- Handle the last field
-    if nb ~= maxNb then
-        result[nb + 1] = string.sub(str, lastPos)
-    end
-    return result
-end
-
 if opt2.save == 1 then
     save_embeddings()
 else
     print('Loading embeddings')
     emb_table = torch.load(opt2.embfile)
-    for _, word in ipairs(Split(opt2.knn, ',')) do
-        print('\n\n== ' .. word .. ' ==\n')
-        input = get_embedding(word)
-        cosined = nn.CosineDistance():cuda()
-        print('Sorting by distance')
-        dist_table = {}
-        for k,v in pairs(emb_table) do
-            dist_table[k] = cosined:forward({input, v:cuda()})[1]
-        end
-        function compare(a, b)
-            return a[2] > b[2]
-        end
-        tmp = {}
-        for k,v in pairs(dist_table) do table.insert(tmp, {k,v}) end
-        table.sort(tmp, compare)
-        for i=1,opt2.k do
-            print(i, tmp[i][1])
-        end
-        print('...')
-        table.sort(tmp, compare)
-        for i=#tmp-opt2.k,#tmp do
-            print(i, tmp[i][1])
-        end
+end
+
+function get_knn(word, num)
+    num = num or 50
+    print('\n== ' .. word .. ' ==\n')
+    input = get_embedding(word)
+    cosined = nn.CosineDistance():cuda()
+    dist_table = {}
+    for k,v in pairs(emb_table) do
+        dist_table[k] = cosined:forward({input, v:cuda()})[1]
+    end
+    function compare(a, b)
+        return a[2] > b[2]
+    end
+    tmp = {}
+    for k,v in pairs(dist_table) do table.insert(tmp, {k,v}) end
+    table.sort(tmp, compare)
+    for i=1,num do
+        print(i, tmp[i][1])
+    end
+    print('...')
+    table.sort(tmp, compare)
+    for i=#tmp-num,#tmp do
+        print(i, tmp[i][1])
     end
 end
